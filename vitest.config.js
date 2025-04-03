c52f6505a609e50c489195355ce4c7412f202abb
Peter Seliger <peter.seliger@googlemail.com>
2025-04-03 18:18:51 +0200

    ci: exclude typedef.js files from code coverage tasks

--- vitest.config.js
+++ vitest.config.js
@@ -6,6 +6,7 @@ export default {
       reporter: ['text', 'text-summary', 'html', 'lcov', 'json'],
       include: ['src/**/*.js'], // Only instrument files under src/
       exclude: [
+        'src/**/typedef.js', // Don’t cover type definition files.
         'src/**/*.test.js', // Don’t cover test files (if placed in src)
         'test/', // Don’t cover anything in your test folder
         'dist/', // Build output
