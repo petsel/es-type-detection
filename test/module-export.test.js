b4fff0522c2901fb16c19152af5577c2d3d22dbc
Peter Seliger <peter.seliger@googlemail.com>
2025-04-09 12:33:42 +0200

    test: refactored test which reflects the new error and function detection features

--- test/module-export.test.js
+++ test/module-export.test.js
@@ -1,6 +1,16 @@
 import { describe, it, expect } from 'vitest';
 
 import {
+  getTypeSignature,
+  getTaggedType,
+  getDefinedConstructor,
+  getDefinedConstructorName,
+  resolveType,
+  defineStableType,
+  hasStableTypeIdentity
+} from '../src/utility';
+
+import {
   isFunction,
   isObject,
   isObjectObject,
@@ -22,65 +32,75 @@ import {
   isBigIntObject
 } from '../src/base';
 
+import { hasOwnPrototype, hasOwnWritablePrototype } from '../src/function/utility';
 import {
-  getTypeSignature,
-  getTaggedType,
-  getDefinedConstructor,
-  getDefinedConstructorName,
-  resolveType,
-  defineStableType,
-  hasStableTypeIdentity
-} from '../src/utility';
+  isClass,
+  isNonAsyncGenerator,
+  isAsyncGenerator,
+  isGenerator,
+  isAsyncFunction,
+  isAsyncNonArrow,
+  isAsyncArrow,
+  isNonAsyncArrow,
+  isArrow,
+  isES3Function,
+  isGenericFunction
+} from '../src/function';
+
+import {
+  isError,
+  isErrorError,
+  isEvalError,
+  isRangeError,
+  isReferenceError,
+  isSyntaxError,
+  isTypeError,
+  isURIError,
+  isAggregateError
+} from '../src/error';
 
 import module from '../src';
+
+function runTestCases(namespace, cases) {
+  for (const [fctName, testFct] of cases) {
+    it(` - \`${fctName}\``, () => {
+      expect(isFunction(namespace[fctName])).toStrictEqual(true);
+      expect(namespace[fctName]).toStrictEqual(testFct);
+    });
+  }
+}
 
 describe('The top most exported module ...', () => {
   it('... is expected to be a plain object (that)...', () => {
     expect(isObjectObject(module)).toStrictEqual(true);
   });
-  it('... features exactly 3 own property keys.', () => {
-    expect(Object.keys(module).length).toStrictEqual(3);
+  it('... features exactly 4 own property keys.', () => {
+    expect(Object.keys(module).length).toStrictEqual(4);
   });
   describe('... should feature the `utility` property ...', () => {
     it('... which itself refers a plain object too ...', () => {
       expect(isObjectObject(module.utility)).toStrictEqual(true);
     });
-    it('... that again features exactly 7 own property keys.', () => {
-      expect(Object.keys(module.utility).length).toStrictEqual(7);
+    it('... that again features exactly 9 own property keys.', () => {
+      expect(Object.keys(module.utility).length).toStrictEqual(9);
     });
     describe('The `utility` namespace should feature following utility methods ...', () => {
-      it(' - `getTypeSignature`', () => {
-        expect(isFunction(module.utility.getTypeSignature)).toStrictEqual(true);
-        expect(module.utility.getTypeSignature).toStrictEqual(getTypeSignature);
-      });
-      it(' - `getTaggedType`', () => {
-        expect(isFunction(module.utility.getTaggedType)).toStrictEqual(true);
-        expect(module.utility.getTaggedType).toStrictEqual(getTaggedType);
-      });
+      runTestCases(module.utility, [
+        ['getTypeSignature', getTypeSignature],
+        ['getTaggedType', getTaggedType],
 
-      it(' - `getDefinedConstructor`', () => {
-        expect(isFunction(module.utility.getDefinedConstructor)).toStrictEqual(true);
-        expect(module.utility.getDefinedConstructor).toStrictEqual(getDefinedConstructor);
-      });
-      it(' - `getDefinedConstructorName`', () => {
-        expect(isFunction(module.utility.getDefinedConstructorName)).toStrictEqual(true);
-        expect(module.utility.getDefinedConstructorName).toStrictEqual(getDefinedConstructorName);
-      });
+        ['getDefinedConstructor', getDefinedConstructor],
+        ['getDefinedConstructorName', getDefinedConstructorName],
 
-      it(' - `resolveType`', () => {
-        expect(isFunction(module.utility.resolveType)).toStrictEqual(true);
-        expect(module.utility.resolveType).toStrictEqual(resolveType);
-      });
+        ['resolveType', resolveType],
 
-      it(' - `defineStableType`', () => {
-        expect(isFunction(module.utility.defineStableType)).toStrictEqual(true);
-        expect(module.utility.defineStableType).toStrictEqual(defineStableType);
-      });
+        ['defineStableType', defineStableType],
 
-      it(' - `hasStableTypeIdentity`', () => {
-        expect(isFunction(module.utility.hasStableTypeIdentity)).toStrictEqual(true);
-        expect(module.utility.hasStableTypeIdentity).toStrictEqual(hasStableTypeIdentity);
-      });
+        ['hasStableTypeIdentity', hasStableTypeIdentity],
+
+        ['hasOwnPrototype', hasOwnPrototype],
+        ['hasOwnWritablePrototype', hasOwnWritablePrototype]
+      ]);
     });
   });
   describe('... should feature the `base` property ...', () => {
@@ -91,88 +111,82 @@ describe('The top most exported module ...', () => {
       expect(Object.keys(module.base).length).toStrictEqual(19);
     });
     describe('The `base` namespace should feature following type detection methods ...', () => {
-      it(' - `isFunction`', () => {
-        expect(isFunction(module.base.isFunction)).toStrictEqual(true);
-        expect(module.base.isFunction).toStrictEqual(isFunction);
-      });
+      runTestCases(module.base, [
+        ['isFunction', isFunction],
 
-      it(' - `isObject`', () => {
-        expect(isFunction(module.base.isObject)).toStrictEqual(true);
-        expect(module.base.isObject).toStrictEqual(isObject);
-      });
-      it(' - `isObjectObject`', () => {
-        expect(isFunction(module.base.isObjectObject)).toStrictEqual(true);
-        expect(module.base.isObjectObject).toStrictEqual(isObjectObject);
-      });
-      it(' - `isDictionaryObject`', () => {
-        expect(isFunction(module.base.isDictionaryObject)).toStrictEqual(true);
-        expect(module.base.isDictionaryObject).toStrictEqual(isDictionaryObject);
-      });
+        ['isObject', isObject],
+        ['isObjectObject', isObjectObject],
+        ['isDictionaryObject', isDictionaryObject],
 
-      it(' - `isString`', () => {
-        expect(isFunction(module.base.isString)).toStrictEqual(true);
-        expect(module.base.isString).toStrictEqual(isString);
-      });
-      it(' - `isStringValue`', () => {
-        expect(isFunction(module.base.isStringValue)).toStrictEqual(true);
-        expect(module.base.isStringValue).toStrictEqual(isStringValue);
-      });
-      it(' - `isStringObject`', () => {
-        expect(isFunction(module.base.isStringObject)).toStrictEqual(true);
-        expect(module.base.isStringObject).toStrictEqual(isStringObject);
-      });
+        ['isString', isString],
+        ['isStringValue', isStringValue],
+        ['isStringObject', isStringObject],
 
-      it(' - `isNumber`', () => {
-        expect(isFunction(module.base.isNumber)).toStrictEqual(true);
-        expect(module.base.isNumber).toStrictEqual(isNumber);
-      });
-      it(' - `isNumberValue`', () => {
-        expect(isFunction(module.base.isNumberValue)).toStrictEqual(true);
-        expect(module.base.isNumberValue).toStrictEqual(isNumberValue);
-      });
-      it(' - `isNumberObject`', () => {
-        expect(isFunction(module.base.isNumberObject)).toStrictEqual(true);
-        expect(module.base.isNumberObject).toStrictEqual(isNumberObject);
-      });
+        ['isNumber', isNumber],
+        ['isNumberValue', isNumberValue],
+        ['isNumberObject', isNumberObject],
 
-      it(' - `isBoolean`', () => {
-        expect(isFunction(module.base.isBoolean)).toStrictEqual(true);
-        expect(module.base.isBoolean).toStrictEqual(isBoolean);
-      });
-      it(' - `isBooleanValue`', () => {
-        expect(isFunction(module.base.isBooleanValue)).toStrictEqual(true);
-        expect(module.base.isBooleanValue).toStrictEqual(isBooleanValue);
-      });
-      it(' - `isBooleanObject`', () => {
-        expect(isFunction(module.base.isBooleanObject)).toStrictEqual(true);
-        expect(module.base.isBooleanObject).toStrictEqual(isBooleanObject);
-      });
+        ['isBoolean', isBoolean],
+        ['isBooleanValue', isBooleanValue],
+        ['isBooleanObject', isBooleanObject],
 
-      it(' - `isSymbol`', () => {
-        expect(isFunction(module.base.isSymbol)).toStrictEqual(true);
-        expect(module.base.isSymbol).toStrictEqual(isSymbol);
-      });
-      it(' - `isSymbolValue`', () => {
-        expect(isFunction(module.base.isSymbolValue)).toStrictEqual(true);
-        expect(module.base.isSymbolValue).toStrictEqual(isSymbolValue);
-      });
-      it(' - `isSymbolObject`', () => {
-        expect(isFunction(module.base.isSymbolObject)).toStrictEqual(true);
-        expect(module.base.isSymbolObject).toStrictEqual(isSymbolObject);
-      });
+        ['isSymbol', isSymbol],
+        ['isSymbolValue', isSymbolValue],
+        ['isSymbolObject', isSymbolObject],
 
-      it(' - `isBigInt`', () => {
-        expect(isFunction(module.base.isBigInt)).toStrictEqual(true);
-        expect(module.base.isBigInt).toStrictEqual(isBigInt);
-      });
-      it(' - `isBigIntValue`', () => {
-        expect(isFunction(module.base.isBigIntValue)).toStrictEqual(true);
-        expect(module.base.isBigIntValue).toStrictEqual(isBigIntValue);
-      });
-      it(' - `isBigIntObject`', () => {
-        expect(isFunction(module.base.isBigIntObject)).toStrictEqual(true);
-        expect(module.base.isBigIntObject).toStrictEqual(isBigIntObject);
-      });
+        ['isBigInt', isBigInt],
+        ['isBigIntValue', isBigIntValue],
+        ['isBigIntObject', isBigIntObject]
+      ]);
+    });
+  });
+  describe('... should feature the `function` property ...', () => {
+    it('... which itself refers a plain object too ...', () => {
+      expect(isObjectObject(module.function)).toStrictEqual(true);
+    });
+    it('... that again features exactly 11 own property keys.', () => {
+      expect(Object.keys(module.function).length).toStrictEqual(11);
+    });
+    describe('The `function` namespace should feature following function-type detection methods ...', () => {
+      runTestCases(module.function, [
+        ['isClass', isClass],
+
+        ['isNonAsyncGenerator', isNonAsyncGenerator],
+        ['isAsyncGenerator', isAsyncGenerator],
+        ['isGenerator', isGenerator],
+
+        ['isAsyncFunction', isAsyncFunction],
+        ['isAsyncNonArrow', isAsyncNonArrow],
+        ['isAsyncArrow', isAsyncArrow],
+
+        ['isNonAsyncArrow', isNonAsyncArrow],
+        ['isArrow', isArrow],
+
+        ['isES3Function', isES3Function],
+        ['isGenericFunction', isGenericFunction]
+      ]);
+    });
+  });
+  describe('... should feature the `error` property ...', () => {
+    it('... which itself refers a plain object too ...', () => {
+      expect(isObjectObject(module.error)).toStrictEqual(true);
+    });
+    it('... that again features exactly 9 own property keys.', () => {
+      expect(Object.keys(module.error).length).toStrictEqual(9);
+    });
+    describe('The `error` namespace should feature following error-type detection methods ...', () => {
+      runTestCases(module.error, [
+        ['isError', isError],
+        ['isErrorError', isErrorError],
+
+        ['isEvalError', isEvalError],
+        ['isRangeError', isRangeError],
+        ['isReferenceError', isReferenceError],
+        ['isSyntaxError', isSyntaxError],
+        ['isTypeError', isTypeError],
+        ['isURIError', isURIError],
+        ['isAggregateError', isAggregateError]
+      ]);
     });
   });
 });
